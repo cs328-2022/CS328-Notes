69f899fd6a37cdd3e3f2e3a231f0476bcac723d9
yadav-sachin <sachinyadav7024@gmail.com>
2022-01-15 14:57:08 +0530

    add deadlines page

---
+++ CS328-Notes/assets/deadlines/get_order.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(14012022)
+groups_df = pd.read_csv("groups.csv")
+groups = groups_df.to_numpy()
+np.random.shuffle(groups)
+ordered_groups_df = pd.DataFrame(groups, columns=groups_df.columns)
+print(ordered_groups_df)
+ordered_groups_df.index += 1
+csv_filename = "ordered_groups.csv"
+ordered_groups_df.to_csv(csv_filename, columns=ordered_groups_df.columns)
